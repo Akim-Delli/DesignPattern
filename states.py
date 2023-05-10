9b905a6b48b4565ec4ac926b24afb0358d056bca
Akim Delli <akim_delli@hotmail.com>
2023-05-09 23:15:22 -0400

    formatting

--- states.py
+++ states.py
@@ -3,8 +3,8 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from time import sleep
 
+
 class State(ABC):
-    
     @property
     def job(self) -> Job:
         return self._job
@@ -12,34 +12,30 @@ class State(ABC):
     @job.setter
     def job(self, job: Job) -> None:
         self._job = job
-    
+
     @abstractmethod
     def notify(self) -> None:
         pass
 
 
-class Started(State):    
-    
+class Started(State):
     def notify(self):
-        print('Job has started')
-        print('Notifying Everybody that Job has started')
+        print("Job has started")
+        print("Notifying Everybody that Job has started")
         self.job.change_state(Running())
 
 
 class Running(State):
-
     def notify(self):
-        print('Job is running')
-        print('No need to notify anybody that job is running, So far so good!')
-        
+        print("Job is running")
+        print("No need to notify anybody that job is running, So far so good!")
+
         sleep(3)
-        
+
         self.job.change_state(Succeeded())
-       
 
 
 class Succeeded(State):
-
     def notify(self):
-        print('Job has succesfully completed')
-        print('Emailing Stakeholders!')
+        print("Job has succesfully completed")
+        print("Emailing Stakeholders!")
